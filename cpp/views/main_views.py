779d76e0b03c65e920420e7f6bd78cc199fdaf77
MyHighIsEpik <vlslrtm2751@gmail.com>
2021-10-28 14:50:50 +0900

    211028 2차

--- cpp/views/main_views.py
+++ cpp/views/main_views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, render_template, flash, request, session
 from werkzeug.utils import redirect
+from cpp.forms import UserLoginForm
+from werkzeug.security import check_password_hash
+from cpp.models import User
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -12,3 +15,21 @@ def hello_pybo():
 @bp.route('/')
 def index():
     return redirect(url_for('program.program'))
+
+
+@bp.route('/mainpage/', methods=('GET', 'POST'))
+def main():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/mainpage.html', form=form)
